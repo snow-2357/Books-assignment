9ea028bf16d625182aa970bb7e71ecefd5a2b555
simantaray <simantaray89@gmail.com>
2024-03-03 10:27:29 +0530

    book delete

--- client/src/components/BookCard.jsx
+++ client/src/components/BookCard.jsx
@@ -1,11 +1,15 @@
 /* eslint-disable react/prop-types */
 // import React from "react";
 
-export default function BookCard({ book }) {
-  console.log(book);
+export default function BookCard({
+  book,
+  openEditModal,
+  openDeleteModal,
+  setCurrentBook,
+}) {
   return (
     <>
-      <div className="max-w-sm rounded overflow-hidden shadow-lg">
+      <div className=" relative max-w-sm rounded overflow-hidden shadow-lg">
         <div className="px-6 py-4">
           <div className=" mb-4 flex flex-col justify-between">
             <h1 className="font-bold text-xl">
@@ -18,13 +22,19 @@ export default function BookCard({ book }) {
 
         <div className="flex justify-between">
           <button
-            type="submit"
+            onClick={() => {
+              setCurrentBook(book);
+              openEditModal();
+            }}
             className="mx-4 m-2 px-8 text-gray-100 bg-blue-500 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm  py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
           >
             Edit
           </button>
           <button
-            type="submit"
+            onClick={() => {
+              setCurrentBook(book);
+              openDeleteModal();
+            }}
             className="mx-4 m-2 px-8 text-gray-100 bg-red-500 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm  py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
           >
             Delete
--- client/src/components/BookList.jsx
+++ client/src/components/BookList.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 import { useState, useEffect, useContext } from "react";
 import axios from "axios";
 
@@ -7,6 +8,17 @@ import { AuthContext } from "../App";
 export default function BookList() {
   const [books, setBooks] = useState([]);
   const { logout } = useContext(AuthContext);
+  const [editModalOpen, setEditModalOpen] = useState(false);
+  const [deleteModalOpen, setDeleteModalOpen] = useState(false);
+  const [currentBook, setCurrentBook] = useState(null);
+  const [refresh, setRefresh] = useState(false);
+  console.log(currentBook);
+
+  const openEditModal = () => setEditModalOpen(true);
+  const closeEditModal = () => setEditModalOpen(false);
+
+  const openDeleteModal = () => setDeleteModalOpen(true);
+  const closeDeleteModal = () => setDeleteModalOpen(false);
 
   useEffect(() => {
     const fetchBooks = async () => {
@@ -21,10 +33,10 @@ export default function BookList() {
     };
 
     fetchBooks();
-  }, []);
+  }, [refresh]);
 
   return (
-    <>
+    <div className="relative">
       <div className="flex w-full justify-end my-4 px-4">
         <button
           onClick={logout}
@@ -37,9 +49,121 @@ export default function BookList() {
       <div className="flex justify-center">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 justify-center">
           {books &&
-            books?.map((book, index) => <BookCard key={index} book={book} />)}
+            books?.map((book, index) => (
+              <BookCard
+                key={index}
+                book={book}
+                openEditModal={openEditModal}
+                openDeleteModal={openDeleteModal}
+                setCurrentBook={(book) => setCurrentBook(book)}
+              />
+            ))}
         </div>
       </div>
-    </>
+      <BookEditModal
+        isOpen={editModalOpen}
+        onClose={closeEditModal}
+        book={currentBook}
+        setRefresh={() => setRefresh((prev) => !prev)}
+      />
+      <BookDeleteModal
+        isOpen={deleteModalOpen}
+        onClose={closeDeleteModal}
+        book={currentBook}
+        setRefresh={() => setRefresh((prev) => !prev)}
+      />
+    </div>
   );
 }
+
+const BookEditModal = ({ isOpen, onClose, book, setRefresh }) => {
+  if (!isOpen) return null;
+
+  return (
+    <div className="absolute top-[-16px] w-full h-screen bg-gray-300 bg-opacity-20">
+      <div className="">
+        <span className=" cursor-pointer" onClick={onClose}>
+          &times;
+        </span>
+        {/* Your edit book form or content here */}
+      </div>
+    </div>
+  );
+};
+
+const BookDeleteModal = ({ isOpen, onClose, book, setRefresh }) => {
+  if (!isOpen) return null;
+
+  const handleDelete = async () => {
+    try {
+      await axios.delete(
+        `${import.meta.env.VITE_BACKEND_URL}/book/${book._id}`
+      );
+
+      onClose();
+      setRefresh();
+    } catch (error) {
+      console.error("Error deleting book:", error);
+    }
+  };
+
+  return (
+    <div className="absolute top-[-16px] w-full h-screen bg-gray-300 bg-opacity-20">
+      <div className="">
+        <div className="flex flex-col bg-white border shadow-sm rounded-xl pointer-events-auto dark:bg-gray-800 dark:border-gray-700 dark:shadow-slate-700/[.7]">
+          <div className="flex justify-between items-center py-3 px-4 dark:border-gray-700">
+            <h3 className="font-bold text-gray-800 dark:text-white">
+              Delete Book
+            </h3>
+            <button
+              onClick={onClose}
+              type="button"
+              className="flex justify-center items-center size-7 text-sm font-semibold rounded-full border border-transparent text-gray-800 hover:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:text-white dark:hover:bg-gray-700 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+              data-hs-overlay="#hs-basic-modal"
+            >
+              <span className="sr-only">Close</span>
+              <svg
+                className="flex-shrink-0 size-4"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <path d="M18 6 6 18" />
+                <path d="m6 6 12 12" />
+              </svg>
+            </button>
+          </div>
+          <div className="p-4 overflow-y-auto">
+            <p className="mt-1 text-gray-800 dark:text-gray-400">
+              Are you sure u want to delete{" "}
+              <span className="font-bold"> {book.title}</span>?
+            </p>
+          </div>
+          <div className="flex justify-end items-center gap-x-2 py-3 px-4 border-t dark:border-gray-700">
+            <button
+              onClick={onClose}
+              type="button"
+              className="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+              data-hs-overlay="#hs-basic-modal"
+            >
+              Close
+            </button>
+            <button
+              onClick={handleDelete}
+              type="button"
+              className="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+            >
+              Yes
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
